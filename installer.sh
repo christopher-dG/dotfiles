61d33f8f7b10e6a8eaa8252563de77ea00308ace
Chris de Graaf <chrisadegraaf@gmail.com>
2019-02-28 03:57:19 -0600

    Quote lots of things

--- installer.sh
+++ installer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 ping google.ca -W1 -c1 &> /dev/null
 if [ $? -ne 0 ]; then
   echo "Connect to the internet first"
@@ -9,25 +11,28 @@ fi
 if [ $1 != chroot ]; then
   timedatectl set-ntp true
 
+  echo -n "Disk (/dev/sdX): "
+  read disk
+
   echo "Create partitions"
-  fdisk /dev/sda
+  fdisk "$disk"
 
   echo -n "Boot partition number: "
   read boot
-  mkfs.vfat -F32 /dev/sda$boot
+  mkfs.vfat -F32 "$disk$boot"
   echo -n "SWAP partition number (empty for none): "
   read swap
-  if [ ! -z $swap ]; then
-    mkswap /dev/sda$swap
-    swapon /dev/sda$swap
+  if [ ! -z "$swap" ]; then
+    mkswap "$disk$swap"
+    swapon "$disk$swap"
   fi
   echo -n "Root partition number: "
   read root
-  mkfs.btrfs /dev/sda$root
+  mkfs.btrfs "$disk$root"
 
-  mount /dev/sda$root /mnt
+  mount "$disk$root" /mnt
   mkdir -p /mnt/boot
-  mount /dev/sda$boot /mnt/boot
+  mount "$disk$boot" /mnt/boot
 
   pacstrap /mnt base base-devel
 
@@ -46,18 +51,17 @@ else
 
   hwclock --systohc
 
-  echo -n "System hostname: "
-  read host
-
   sed -i "s/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/" /etc/locale.gen
   locale-gen
   echo LANG=en_US.UTF-8 > /etc/locale.conf
 
-  echo $host > /etc/hostname
+  echo -n "System hostname: "
+  read host
+  echo "$host" > /etc/hostname
 
   echo -n "Root password: "
   read rootpw
-  echo root:$rootpw | chpasswd
+  echo "root:$rootpw" | chpasswd
 
   grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
   grub-mkconfig -o /boot/grub/grub.cfg
@@ -69,23 +73,30 @@ else
   reflector -n5 -f5 --save /etc/pacman.d/mirrorlist
 
   pacman -Syu
-  pacman -Sy efibootmgr git grub intel-ucode zsh
+  pacman -Sy efibootmgr git grub intel-ucode rsync zsh
 
   sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/" /etc/sudoers
   chsh -s /bin/zsh
 
   echo -n "Username: "
   read user
-  useradd $user -G wheel -s /bin/zsh
+  useradd "$user" -G wheel -s /bin/zsh
   echo -n "User password: "
   read userpw
-  echo $user:$userpw | chpasswd
+  echo "$user:$userpw" | chpasswd
 
-  git clone https://github.com/christopher-dG/dotfiles /home/$user
-  rsync -a /home/$user/overlay/ /
-  chown $user:$user -R /home/$user
+  home="/home/$user"
+  git clone https://github.com/christopher-dG/dotfiles "$home"
+  rsync -a "$home/overlay/" /
+  chown -R "$user:$user" "$home"
 
-  curl -L -o yay.tar.gz https://github.com/Jguer/yay/releases/download/v9.1.0/yay_9.1.0_x86_64.tar.gz
+  if [ -z "$(lspci | grep -i nvidia)" ]; then
+    pacman -Sy xf86-video-intel
+  else
+    pacman -Sy nvidia
+  fi
+
+  curl -L -o yay.tar.gz "https://github.com/Jguer/yay/releases/download/v9.1.0/yay_9.1.0_x86_64.tar.gz"
   tar xf yay.tar.gz
   ./yay_9.1.0_x86_64/yay -S yay-bin
   rm -rf yay.tar.gz yay_9.1.0_x86_64
@@ -138,7 +149,6 @@ else
       ranger \
       redshift \
       ripgrep \
-      rsync \
       rust-racer \
       rustup \
       rxvt-unicode \
@@ -160,7 +170,6 @@ else
       wget \
       xdotool \
       xf86-input-libinput \
-      xf86-video-intel \
       xorg-server \
       xorg-xbacklight \
       xorg-xinit \
@@ -170,18 +179,17 @@ else
 
   systemctl enable NetworkManager ntpd
 
-  usermod degraafc -a -G docker,input,vboxusers
+  usermod "$user" -a -G docker,input,vboxusers
 
-  sudo -u $user sh -c "
-  git -C /home/$user remote set-url origin git@github.com:christopher-dG/dotfiles.git
+  sudo -u "$user" sh -c "
+  git -C $home remote set-url origin git@github.com:christopher-dG/dotfiles.git
 
-  find /home/$user/overlay -type f | xargs git assume-unchanged
-  git assume-unchanged /home/$user/installer.sh
-  rm -rf /home/$user/{installer.sh,overlay}
+  find $home/overlay -type f | xargs git assume-unchanged $home/installer.sh
+  rm -rf $home/{installer.sh,overlay}
 
   curl -L https://raw.github.com/zachcurry/emacs-anywhere/master/install | bash
 
-  git clone https://github.com/asdf-vm/asdf.git /home/$user/.asdf --branch v0.6.3
+  git clone https://github.com/asdf-vm/asdf.git $home/.asdf --branch v0.6.3
 
   go get golang.org/x/tools/cmd/goimports
   go get github.com/stamblerre/gocode
@@ -189,8 +197,8 @@ else
   rustup install stable nightly
   rustup component add rust-src
 
-  mkdir -p /home/$user/.ssh
-  ssh-keygen -N"" -f /home/$user/.ssh/id_rsa
+  mkdir -p $home/.ssh
+  ssh-keygen -N"" -f $home/.ssh/id_rsa
 
   systemctl --user enable emacs
   systemctl --user enable spotifyd
