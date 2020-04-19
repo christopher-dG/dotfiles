4db9677c1767bc07ee88cc2062e98150654197c1
Chris de Graaf <chrisadegraaf@gmail.com>
2020-04-18 20:04:32 -0500

    SharpKeys config

--- .config/jupyter/jupyter_notebook_config.py
+++ .config/jupyter/jupyter_notebook_config.py
@@ -4,3 +4,4 @@ c.NotebookApp.ip = "0.0.0.0"
 c.NotebookApp.notebook_dir = os.path.expanduser("~")
 c.NotebookApp.port = 8888
 c.NotebookApp.password = "sha1:354ceb9afa77:97e42741d31a430371ae47b6147ed94c970f7b97"
+c.FileCheckpoints.checkpoint_dir = os.path.expanduser("~/.local/share/jupyter/checkpoints")
